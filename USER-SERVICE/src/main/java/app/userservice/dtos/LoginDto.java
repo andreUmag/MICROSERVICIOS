74e5d5bbdbccb203b8149eb3c0aecb05a3c8c12f
Andres <142335765+andreUmag@users.noreply.github.com>
2024-09-18 20:10:43 -0500

    User service add

---
+++ USER-SERVICE/src/main/java/app/userservice/dtos/LoginDto.java
@@ -0,0 +1,11 @@
+package app.userservice.dtos;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+
+    private String username;
+
+    private String password;
+}
---
+++ USER-SERVICE/src/main/java/app/userservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package app.userservice.repository;
+
+import app.userservice.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Usuario, Long> {
+    Usuario findByUsername(String username);
+}
