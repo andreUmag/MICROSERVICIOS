9a765ff76ce4571152a4b1e221973ec10adbf0de
Andres <142335765+andreUmag@users.noreply.github.com>
2024-10-02 18:23:58 -0500

    service comunication services add

---
+++ prodcutservice/src/test/java/app/prodcutservice/ProdcutserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.prodcutservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdcutserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
